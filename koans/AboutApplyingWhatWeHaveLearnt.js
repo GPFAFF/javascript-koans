53719d9f4c3931cb15067941f8a8d6ce6dee4149
Greg Pfaff <greg.pfaff@razorfish.com>
2016-04-14 17:03:27 -0500

    adding koans practice for JS

--- koans/AboutApplyingWhatWeHaveLearnt.js
+++ koans/AboutApplyingWhatWeHaveLearnt.js
@@ -32,16 +32,18 @@ describe("About Applying What We Have Learnt", function() {
         }
     }
 
-    expect(productsICanEat.length).toBe(FILL_ME_IN);
+    expect(productsICanEat.length).toBe(1);
   });
 
   it("given I'm allergic to nuts and hate mushrooms, it should find a pizza I can eat (functional)", function () {
 
-      var productsICanEat = [];
+      productsICanEat = (products).filter(function(product){
+        return product.containsNuts === false && product.ingredients.indexOf("mushrooms") === -1
+      })
 
       /* solve using filter() & all() / any() */
 
-      expect(productsICanEat.length).toBe(FILL_ME_IN);
+      expect(productsICanEat.length).toBe(1);
   });
 
   /*********************************************************************************/
@@ -55,12 +57,23 @@ describe("About Applying What We Have Learnt", function() {
       }
     }
 
-    expect(sum).toBe(FILL_ME_IN);
+    expect(sum).toBe(233168);
   });
 
   it("should add all the natural numbers below 1000 that are multiples of 3 or 5 (functional)", function () {
 
-    var sum = FILL_ME_IN;    /* try chaining range() and reduce() */
+    var sum = 0;    /* try chaining range() and reduce() */
+
+   
+      var ranger = _.range(1000, sum)
+      console.log(ranger);
+
+      _.reduce(ranger, function(memo,num){
+        
+          console.log(num)
+        
+      })
+     
 
     expect(233168).toBe(FILL_ME_IN);
   });
